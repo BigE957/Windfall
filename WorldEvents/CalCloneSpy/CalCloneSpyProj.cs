b616dc65d5bbc34139a83e95ecf80ce727a86353
Big E <evanmorrison42@gmail.com>
2024-01-02 23:00:22 -0500

    + Added the first World Event ~Calamitas Clone will rarely be found spying outside the Player's town.

--- WorldEvents/CalCloneSpy/CalCloneSpyProj.cs
+++ WorldEvents/CalCloneSpy/CalCloneSpyProj.cs
@@ -1,36 +1,129 @@
-﻿using CalamityMod.Items.Placeables.Banners;
-using CalamityMod;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria.ID;
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
+using Terraria.Localization;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using System;
+using Terraria.ID;
+using Terraria.Audio;
 
 namespace WindfallAttempt1.WorldEvents.CalCloneSpy
 {
     public class CalCloneSpyProj : ModProjectile
-
     {
+        public static readonly SoundStyle DashSound = new("CalamityMod/Sounds/Custom/SCalSounds/SCalDash");
+        public static readonly SoundStyle CalCloneTeleport = new("CalamityMod/Sounds/Custom/SCalSounds/BrimstoneHellblastSound");
+        public enum AIState
+        {
+            WaitingForPlayer,
+            Shocked,
+            Fleeing,
+        }
+        public float i = 0f;
+        public int timeTillSpawn;
+        public AIState CurrentAI
+        {
+            get => (AIState)Projectile.ai[0];
+            set => Projectile.ai[0] = (int)value;
+        }
         public override void SetStaticDefaults()
         {
             // DisplayName.SetDefault("Calamitas");
-            Main.projFrames[Type] = 7;
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 4;
+            Main.projFrames[Type] = 6;
         }
 
         public override void SetDefaults()
         {
-            Projectile.width = Projectile.height = 50;
-            Projectile.hostile = true;
+            Projectile.width = 50;
+            Projectile.height = 60;
             Projectile.tileCollide = true;
             Projectile.ignoreWater = true;
             Projectile.netImportant = true;
             Projectile.penetrate = -1;
             Projectile.timeLeft = int.MaxValue;
+        }
+        public override void AI()
+        {
+            if (Main.dayTime)
+            {
+                Projectile.Kill();
+            }
+
+            if (++Projectile.frameCounter >= 5)
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame = ++Projectile.frame % Main.projFrames[Projectile.type];
+            }
+
+            Player closestPlayer = Main.player[Player.FindClosest(Projectile.Center, 1, 1)];
+            Projectile.spriteDirection = Projectile.direction = (closestPlayer.Center.X > Projectile.Center.X).ToDirectionInt();
+            
+            Lighting.AddLight(Projectile.Center, 0.9f, 0.1f, 0.3f);
+
+            switch (CurrentAI)
+            {
+                case AIState.WaitingForPlayer:
+                    if (Projectile.WithinRange(closestPlayer.Center, 320f))
+                    {
+                        CurrentAI = AIState.Shocked;
+                        Projectile.velocity.Y = 0;
+                        if (closestPlayer.Center.X > Projectile.Center.X)
+                        {
+                            i = -20;
+                        }
+                        else
+                        {
+                            i = 20;
+                        }
+                        SoundEngine.PlaySound(DashSound, Projectile.Center);
+                    }
+                    else
+                    {
+                        i++;
+                        Projectile.velocity.Y = (float)(Math.Sin(i/10)*1);
+                    }
+                    break;
+                case AIState.Shocked:
+                    if (i > 0)
+                    {
+                        Projectile.velocity.X = i--/2;
+                    }
+                    else if(i < 0)
+                    {
+                        Projectile.velocity.X = i++/2;
+                    }
+                    else
+                    {
+                        i = 30;
+                        Projectile.velocity.X = 0;
+                        CurrentAI = AIState.Fleeing;
+                        Color messageColor = Color.DarkRed;
+                        Rectangle location = new Rectangle((int)Projectile.Center.X, (int)Projectile.Center.Y, Projectile.width, Projectile.width);
+                        CombatText.NewText(location, messageColor, "?!", true);
+                    }
+
+                    break;
+                case AIState.Fleeing:
+                    i--;
+                    if (i == 0)
+                    {
+                        for (int i = 0; i < 50; i++)
+                        {
+                            Vector2 speed = Main.rand.NextVector2Circular(0.5f, 1f);
+                            Dust d = Dust.NewDustPerfect(Projectile.Center, DustID.Blood, speed * 5, Scale: 1.5f);
+                            d.noGravity = true;
+                        }
+                        SoundEngine.PlaySound(CalCloneTeleport, Projectile.Center);
+                        Projectile.Kill();
+                    }
+
+                    break;
+            }
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.position.Y -= 16; 
+            return false;
         }
     }
 }
---
+++ WorldEvents/CalCloneSpy/CalCloneSpySpawnSystem.cs
@@ -0,0 +1,83 @@
+﻿using CalamityMod.NPCs.Signus;
+using CalamityMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Terraria.Localization;
+using System.Threading;
+
+namespace WindfallAttempt1.WorldEvents.CalCloneSpy
+{
+    public class CalCloneSpySpawnSystem : ModSystem
+    {
+        //used to stop Calamitas from spawning regardless of other conditions. serves as a cooldown so she only spawns once a night
+        public int CalDown = 0;
+        public double timeTillSpawn;
+        public override void PreUpdateWorld()
+        {
+            Player mainPlayer = Main.player[0];
+            //Requirements for Calamitas to spawn.
+            if (!Main.hardMode || Main.dayTime || CalDown == 1)
+            {
+                if (Main.dayTime)
+                {
+                    //resets the cooldown when day comes.
+                    CalDown = 0;
+                }
+                return;
+            }else if (!Main.rand.NextBool(5) && CalDown != 2)
+            {
+                // 1/5 chance for the spawn to go through
+                CalDown = 1;
+                return;
+            }
+            else
+            {
+                if (CalDown != 2)
+                {
+                    Main.NewText("Something calamitous is approaching...", Color.Red);
+                    timeTillSpawn = Main.nightLength / Main.rand.Next(2, 4);
+                    CalDown = 2;
+                }
+                if (Main.time > timeTillSpawn && mainPlayer.townNPCs > 2f && !mainPlayer.dead && mainPlayer.active && !CalamityUtils.AnyBossNPCS())
+                    {
+                        Main.NewText("Something calamitous has awoken!", Color.Red);
+                        //sets the spawn location 
+                        Vector2 CalCloneSpawnLocation;
+                        if (mainPlayer.direction == 1)
+                        {
+                            CalCloneSpawnLocation.X = mainPlayer.Center.X + 1000f;
+                        }
+                        else
+                        {
+                            CalCloneSpawnLocation.X = mainPlayer.Center.X - 1000f;
+                        }
+
+                        CalCloneSpawnLocation.Y = mainPlayer.Center.Y;
+                        for (int i = 0; i < Main.maxPlayers; i++)
+                        {
+                            Player p = Main.player[i];
+                            if (!p.dead && p.active)
+                            {
+                                Utilities.NewProjectileBetter(CalCloneSpawnLocation, Vector2.Zero, ModContent.ProjectileType<CalCloneSpyProj>(), 0, 0f);
+                                CalDown = 1;
+                                break;
+                            }
+                        }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
