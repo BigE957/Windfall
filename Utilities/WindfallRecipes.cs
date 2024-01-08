c95bb366457949d5a001cdbadf08fe4472f2ad39
Big E <evanmorrison42@gmail.com>
2024-01-07 22:11:16 -0500

    + Added 5 Ashes of Calamity to the Eye of Desolation recipe

---
+++ Utilities/WindfallRecipes.cs
@@ -0,0 +1,27 @@
+﻿using CalamityMod.Items.Weapons.Magic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using CalamityMod.Items.Materials;
+
+namespace WindfallAttempt1.Utilities
+{
+    public class WindfallRecipes : ModSystem
+    {
+        public override void PostAddRecipes()
+        {
+            for (int i = 0; i < Recipe.numRecipes; i++)
+            {
+                Recipe recipe = Main.recipe[i];
+                if (recipe.HasResult(ModContent.ItemType<TearsofHeaven>()))
+                {
+                    recipe.AddIngredient(ModContent.ItemType<AshesofCalamity>(), 5);
+                }
+            }
+        }
+    }
+}
