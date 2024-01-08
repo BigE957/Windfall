a8ea8ab2e4ba31ae54ee438d8ebc2761370ba2d1
Big E <evanmorrison42@gmail.com>
2024-01-07 22:12:32 -0500

    + Fixed issue where Ashes of Calamity were added to the Tears of Heaven recipe instead of the Eye of Desolation

--- Utilities/WindfallRecipes.cs
+++ Utilities/WindfallRecipes.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader;
 using CalamityMod.Items.Materials;
+using CalamityMod.Items.SummonItems;
 
 namespace WindfallAttempt1.Utilities
 {
@@ -17,7 +18,7 @@ namespace WindfallAttempt1.Utilities
             for (int i = 0; i < Recipe.numRecipes; i++)
             {
                 Recipe recipe = Main.recipe[i];
-                if (recipe.HasResult(ModContent.ItemType<TearsofHeaven>()))
+                if (recipe.HasResult(ModContent.ItemType<EyeofDesolation>()))
                 {
                     recipe.AddIngredient(ModContent.ItemType<AshesofCalamity>(), 5);
                 }
