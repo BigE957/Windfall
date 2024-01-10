0fa05221441ee7d5c5c6555af7dc7944f8f4156d
Big E <evanmorrison42@gmail.com>
2024-01-10 16:56:19 -0500

    + Added the Boneripper

---
+++ Items/Weapons/Melee/Boneripper.cs
@@ -0,0 +1,35 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using CalamityMod.Items;
+using WindfallAttempt1.Projectiles.Melee;
+using CalamityMod.Items.Weapons.Melee;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace WindfallAttempt1.Items.Weapons.Melee
+{
+    public class Boneripper : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Weapons.Melee";
+        public override void SetDefaults()
+        {
+            Item.damage = 1075;
+            Item.knockBack = 7.5f;
+            Item.useAnimation = Item.useTime = 25;
+            Item.DamageType = DamageClass.MeleeNoSpeed;
+            Item.noMelee = true;
+            Item.channel = true;
+            Item.autoReuse = true;
+            Item.shootSpeed = 14f;
+            Item.shoot = ModContent.ProjectileType<BoneripperProj>();
+
+            Item.width = 90;
+            Item.height = 90;
+            Item.noUseGraphic = true;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.UseSound = SoundID.DD2_SkyDragonsFurySwing;
+            Item.value = CalamityGlobalItem.Rarity7BuyPrice;
+            Item.rare = ItemRarityID.Lime;
+        }
+    }
+}
