56f9b9b1c830308e803a443396aaf9fd7228eff9
Big E <evanmorrison42@gmail.com>
2024-01-29 20:05:05 -0500

    + Ah, free... At last...

--- Systems/DownedNPCSystem.cs
+++ Systems/DownedNPCSystem.cs
@@ -13,6 +13,7 @@ namespace Windfall.Systems
     {
         internal static bool _downedCnidrion = false;
         internal static bool _downedSirenLure = false;
+        internal static bool _downedEvil2Summon = false;
 
         public static bool downedCnidrion
         {
@@ -36,10 +37,22 @@ namespace Windfall.Systems
                     NPC.SetEventFlagCleared(ref _downedSirenLure, -1);
             }
         }
+        public static bool downedEvil2Summon
+        {
+            get => _downedEvil2Summon;
+            set
+            {
+                if (!value)
+                    _downedEvil2Summon = false;
+                else
+                    NPC.SetEventFlagCleared(ref _downedEvil2Summon, -1);
+            }
+        }
         internal static void ResetAllFlags()
         {
             downedCnidrion = false;
             downedSirenLure = false;
+            downedEvil2Summon = false;
         }
         public override void OnWorldLoad() => ResetAllFlags();
 
@@ -56,6 +69,10 @@ namespace Windfall.Systems
             if (downedSirenLure)
                 downed.Add("SirenLure");
             tag["downedFlags"] = downed;
+
+            if (downedEvil2Summon)
+                downed.Add("Evil2Summon");
+            tag["downedFlags"] = downed;
         }
         public override void LoadWorldData(TagCompound tag)
         {
@@ -63,6 +80,7 @@ namespace Windfall.Systems
 
             downedCnidrion = downed.Contains("Cnidrion");
             downedSirenLure = downed.Contains("SirenLure");
+            downedEvil2Summon = downed.Contains("Evil2Summon");
         }
     }
 }
