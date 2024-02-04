c611643ec95f379a9690124e5b6efd9524d3db27
Big E <evanmorrison42@gmail.com>
2024-02-04 10:18:46 -0500

    + Readded Debug Items

---
+++ Content/Items/Debug/QuestsEnd.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using CalamityMod.Rarities;
+using Windfall.Content.Systems;
+
+namespace Windfall.Items.Debug
+{
+    public class QuestsEnd : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Debug";
+        public override string Texture => "CalamityMod/Items/Weapons/Rogue/BallisticPoisonBomb";
+        public override void SetDefaults()
+        {
+            Item.width = 25;
+            Item.height = 29;
+            Item.rare = ItemRarityID.Red;
+            Item.useAnimation = Item.useTime = 20;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+        }
+        public override bool? UseItem(Player player)
+        {
+            QuestSystem.QuestLog = QuestSystem.InitializedQuestLog();
+            return true;
+        }
+    }
+}
