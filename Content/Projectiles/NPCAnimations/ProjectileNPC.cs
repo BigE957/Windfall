b1ee9de3f51a9bf8493b86891ae1528ed80c7a6a
Big E <evanmorrison42@gmail.com>
2024-02-15 20:33:14 -0500

    + Created a new abstract class to handle Projectiles which mimick NPCs and use Combat Text (Statis and Lunar Bishop both being examples of this)

---
+++ Content/Projectiles/NPCAnimations/ProjectileNPC.cs
@@ -0,0 +1,108 @@
+﻿using CalamityMod.Dusts;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Windfall.Common.Utilities;
+
+namespace Windfall.Content.Projectiles.NPCAnimations
+{
+    public abstract class ProjectileNPC : ModProjectile
+    {
+        private enum AIState
+        {
+            SpawnDelay,
+            Yapping,
+            TurnToNPC,
+        }
+        private AIState CurrentAI
+        {
+            get => (AIState)Projectile.ai[0];
+            set => Projectile.ai[0] = (int)value;
+        }
+        private int counter
+        {
+            get => (int)Projectile.ai[1];
+            set => Projectile.ai[1] = value;
+        }
+        int dialogueCounter = 0;
+        internal struct dialogue
+        {
+            internal string text;
+            internal int delay;
+        }
+        internal virtual List<dialogue> MyDialogue => new();
+        internal virtual SoundStyle SpawnSound => new();
+        internal virtual int NPCType => 0;
+        internal virtual Color TextColor => new(); 
+        public override void OnSpawn(IEntitySource source)
+        {
+            Player closestPlayer = Main.player[Player.FindClosest(Projectile.Center, 1, 1)];
+
+            if (!Utilities.AlignProjectileWithGround(Main.projectile[Projectile.whoAmI]))
+                Projectile.position.Y = closestPlayer.position.Y;
+        }
+        public override void AI()
+        {
+            Player closestPlayer = Main.player[Player.FindClosest(Projectile.Center, 1, 1)];
+            Projectile.spriteDirection = Projectile.direction = (closestPlayer.Center.X > Projectile.Center.X).ToDirectionInt() * -1;
+            switch (CurrentAI)
+            {
+                case AIState.SpawnDelay:
+                    Vector2 vectorFromNpcToPlayer = closestPlayer.Center - Projectile.Center;
+                    float distance = vectorFromNpcToPlayer.Length();
+                    if (counter >= 60 * 4 && distance < 250f)
+                    {
+                        dialogueCounter = 0;
+                        for (int i = 0; i < 75; i++)
+                        {
+                            Vector2 speed = Main.rand.NextVector2Circular(1.5f, 2f);
+                            Dust d = Dust.NewDustPerfect(Projectile.Center, (int)CalamityDusts.PurpleCosmilite, speed * 4, Scale: 1.5f);
+                            d.noGravity = true;
+                        }
+                        SoundEngine.PlaySound(SpawnSound, Projectile.Center);
+                        Projectile.alpha = 0;
+                        counter = 60 * 4;
+                        CurrentAI = AIState.Yapping;
+                    }
+                    break;
+                case AIState.Yapping:
+                    if (MyDialogue.Count == dialogueCounter)
+                    {
+                        CurrentAI = AIState.TurnToNPC;
+                        break;
+                    }
+                    int Delay = 0;
+                    for (int i = dialogueCounter; i >= 0; i--)
+                    {
+                        Delay += MyDialogue[i].delay;
+                    }
+                    if (counter == (60 * 4) + (60 * Delay))
+                    {
+                        DisplayMessage(MyDialogue[dialogueCounter].text, Main.projectile[Projectile.whoAmI], TextColor);
+                        dialogueCounter++;
+                    }
+                    break;
+                case AIState.TurnToNPC:
+                    NPC.NewNPC(NPC.GetSource_NaturalSpawn(), (int)Projectile.Center.X, (int)Projectile.Bottom.Y-1, NPCType, 0, Projectile.velocity.Y, Projectile.direction);
+                    Projectile.active = false;
+                    break;
+
+            }
+            counter++;
+        }
+
+        internal static void DisplayMessage(string text, Projectile projectile, Color color)
+        {
+            Rectangle location = new((int)projectile.Center.X, (int)projectile.Center.Y, projectile.width, projectile.width);
+            CombatText MyDialogue = Main.combatText[CombatText.NewText(location, color, text, true)];
+            if (MyDialogue.text.Length > 50)
+            {
+                MyDialogue.lifeTime = 60 + MyDialogue.text.Length;
+            }
+        }
+    }
+}
--- Content/Projectiles/NPCAnimations/StatisProj.cs
+++ Content/Projectiles/NPCAnimations/StatisProj.cs
@@ -12,37 +12,15 @@ using Terraria.ModLoader;
 using Windfall.Common.Systems;
 using Windfall.Common.Utilities;
 using Windfall.Content.NPCs.WanderingNPCs;
+using Windfall.Content.NPCs.WorldEvents.LunarCult;
 
 namespace Windfall.Content.Projectiles.NPCAnimations
 {
-    public class StatisProj : ModProjectile, ILocalizedModType
+    public class StatisProj : ProjectileNPC, ILocalizedModType
     {
         public override string Texture => "Windfall/Assets/Projectiles/NPCAnimations/LoneRoninTeleport";
-        private static readonly SoundStyle Teleport = new("CalamityMod/Sounds/Custom/SCalSounds/BrimstoneHellblastSound");
-
-        private enum AIState
+        internal override List<dialogue> MyDialogue => new()
         {
-            SpawnDelay,
-            Yapping,
-            TurnToNPC,
-        }
-        private AIState CurrentAI
-        {
-            get => (AIState)Projectile.ai[0];
-            set => Projectile.ai[0] = (int)value;
-        }
-        private int counter
-        {
-            get => (int)Projectile.ai[1];
-            set => Projectile.ai[1] = value;
-        }
-        internal struct dialogue
-        {
-            internal string text;
-            internal int delay;
-        }
-        internal static List<dialogue> StatisDialogue = new()
-        { 
             new dialogue {text = "Quite the display..." , delay = 1},
             new dialogue {text = "I'll admit I hadn't thought you'd survive that... thing. Let alone defeat it." , delay = 3},
             new dialogue {text = "Even in such a state, the might of the deities is ever formidable..." , delay = 3},
@@ -51,7 +29,10 @@ namespace Windfall.Content.Projectiles.NPCAnimations
             new dialogue {text = "Your help might just prove invaluably in putting a stop to it." , delay = 3},
             new dialogue {text = "If you'd be willing, I'd be ever thankful." , delay = 3},
         };
-        int dialogueCounter = 0;
+        internal override SoundStyle SpawnSound => new("CalamityMod/Sounds/Custom/SCalSounds/BrimstoneHellblastSound");
+        internal override int NPCType => ModContent.NPCType<LoneRonin>();
+        internal override Color TextColor => Color.DeepPink;
+
         public override void SetStaticDefaults()
         {
             Main.projFrames[Type] = 1;
@@ -59,7 +40,7 @@ namespace Windfall.Content.Projectiles.NPCAnimations
         public override void SetDefaults()
         {
             Projectile.width = 26;
-            Projectile.height = 40;
+            Projectile.height = 42;
             Projectile.tileCollide = false;
             Projectile.ignoreWater = true;
             Projectile.netImportant = true;
@@ -67,70 +48,6 @@ namespace Windfall.Content.Projectiles.NPCAnimations
             Projectile.timeLeft = int.MaxValue;
             Projectile.alpha = 255;
             Projectile.ai[0] = 0;
-
-        }
-        public override void OnSpawn(IEntitySource source)
-        {
-            Player closestPlayer = Main.player[Player.FindClosest(Projectile.Center, 1, 1)];
-            
-            if (!Utilities.AlignProjectileWithGround(Main.projectile[Projectile.whoAmI]))
-                Projectile.position.Y = closestPlayer.position.Y;
-            Projectile.position.Y -= 3;
-        }
-        public override void AI()
-        {
-            Player closestPlayer = Main.player[Player.FindClosest(Projectile.Center, 1, 1)];
-            Projectile.spriteDirection = Projectile.direction = (closestPlayer.Center.X > Projectile.Center.X).ToDirectionInt() * -1;
-            switch (CurrentAI)
-            {
-                case AIState.SpawnDelay:
-                    if(counter == 60 * 4)
-                    {
-                        dialogueCounter = 0;
-                        for (int i = 0; i < 75; i++)
-                        {
-                            Vector2 speed = Main.rand.NextVector2Circular(1.5f, 2f);
-                            Dust d = Dust.NewDustPerfect(Projectile.Center, (int)CalamityDusts.PurpleCosmilite, speed * 4, Scale: 1.5f);
-                            d.noGravity = true;
-                        }
-                        SoundEngine.PlaySound(Teleport, Projectile.Center);
-                        Projectile.alpha = 0;
-                        CurrentAI = AIState.Yapping;
-                    }
-                    break;
-                case AIState.Yapping:
-                    if (StatisDialogue.Count == dialogueCounter)
-                    {
-                        CurrentAI = AIState.TurnToNPC;
-                        break;
-                    }
-                    int Delay = 0;
-                    for (int i = dialogueCounter; i >= 0; i--)
-                    {
-                        Delay += StatisDialogue[i].delay;
-                    }
-                    if (counter == (60 * 4) + (60 * Delay))
-                    {                       
-                        StatisMessage(StatisDialogue[dialogueCounter].text, Main.projectile[Projectile.whoAmI]);
-                        dialogueCounter++;
-                    }
-                    break;
-                case AIState.TurnToNPC:
-                    NPC.NewNPC(NPC.GetSource_NaturalSpawn(), (int)Projectile.Center.X, (int)Projectile.Bottom.Y, ModContent.NPCType<LoneRonin>(), 0, Projectile.velocity.Y, Projectile.direction);
-                    Projectile.active = false;
-                    break;
-
-            }
-            counter++;
-        }
-        internal static void StatisMessage(string text, Projectile Statis)
-        {
-            Rectangle location = new((int)Statis.Center.X, (int)Statis.Center.Y, Statis.width, Statis.width);
-            CombatText MyDialogue = Main.combatText[CombatText.NewText(location, Color.DeepPink, text, true)];
-            if (MyDialogue.text.Length > 50)
-            {
-                MyDialogue.lifeTime = 60 + MyDialogue.text.Length;
-            }
         }
     }
 }
