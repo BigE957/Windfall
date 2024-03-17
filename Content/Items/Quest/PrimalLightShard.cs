ff6b1a9ab64147f3fd60bb68b0f6a586385e8094
Big E <evanmorrison42@gmail.com>
2024-03-17 13:37:56 -0400

    + Added suprise tools which'll help us later

---
+++ Content/Items/Quest/PrimalLightShard.cs
@@ -0,0 +1,18 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Windfall.Content.Items.Quests
+{
+    public class PrimalLightShard : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Quest";
+        public override string Texture => "Windfall/Assets/Items/Quest/DeificInsignia";
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 24;
+            Item.rare = ItemRarityID.Green;
+            Item.maxStack = 99;
+        }
+    }
+}
