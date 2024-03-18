6f4c5bc29ff9869d3bbe38614bde15679bf97fc3
Big E <evanmorrison42@gmail.com>
2024-03-18 17:12:16 -0400

    + Added the Fishron Bone as a Tablet Componant

--- Content/Items/Quest/DraconicBone.cs
+++ Content/Items/Quest/DraconicBone.cs
@@ -6,13 +6,13 @@ namespace Windfall.Content.Items.Quests
     public class DraconicBone : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Quest";
-        public override string Texture => "Windfall/Assets/Items/Quest/DeificInsignia";
+        public override string Texture => "Windfall/Assets/Items/Quest/DraconicBone";
         public override void SetDefaults()
         {
             Item.width = 22;
             Item.height = 24;
-            Item.rare = ItemRarityID.Green;
-            Item.maxStack = 99;
+            Item.rare = ItemRarityID.Quest;
+            Item.maxStack = 1;
         }
     }
 }
