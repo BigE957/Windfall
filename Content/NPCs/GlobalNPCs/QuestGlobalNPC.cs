efc97df79258298bfde51aa8d2dde9c0e929b142
Big E <evanmorrison42@gmail.com>
2024-04-11 17:34:04 -0400

    + Moved all Quest related code to QuestGlobalNPC as opposed to being within the WindfallGlobalNPC

--- Content/NPCs/GlobalNPCs/QuestGlobalNPC.cs
+++ Content/NPCs/GlobalNPCs/QuestGlobalNPC.cs
@@ -5,32 +5,21 @@ using Windfall.Content.Projectiles.Other;
 
 namespace Windfall.Content.NPCs.GlobalNPCs
 {
-    public class WindfallGlobalNPC : GlobalNPC
+    public class QuestGlobalNPC : GlobalNPC
     {
         public override void OnKill(NPC npc)
         {
             Mod calamity = ModLoader.GetMod("CalamityMod");
             Mod windfall = Windfall.Instance;
-            if (npc.type == calamity.Find<ModNPC>("Cnidrion").Type || npc.type == ModContent.NPCType<WFCnidrion>())
-                DownedNPCSystem.downedCnidrion = true;
 
-            if((npc.type == NPCID.EaterofSouls || npc.type == NPCID.Crimera) && QuestSystem.IsQuestActive("PestControl"))
+            if ((npc.type == NPCID.EaterofSouls || npc.type == NPCID.Crimera) && QuestSystem.QuestLog[QuestSystem.QuestLog.FindIndex(quest => quest.Name == "PestControl")].Active)
                 QuestSystem.IncrementQuestProgress(QuestSystem.QuestLog.FindIndex(quest => quest.Name == "PestControl"), 0);
-
-            if (npc.type == calamity.Find<ModNPC>("PerforatorCyst").Type || npc.type == calamity.Find<ModNPC>("HiveTumor").Type)
-                DownedNPCSystem.downedEvil2Summon = true;
 
             if (npc.type == calamity.Find<ModNPC>("DesertScourgeHead").Type)
                 QuestSystem.IncrementQuestProgress(QuestSystem.QuestLog.FindIndex(quest => quest.Name == "ScoogHunt"), 0);
 
             if (npc.type == calamity.Find<ModNPC>("GiantClam").Type)
                 QuestSystem.IncrementQuestProgress(QuestSystem.QuestLog.FindIndex(quest => quest.Name == "ClamHunt"), 0);
-
-            if (npc.type == calamity.Find<ModNPC>("HiveMind").Type && !DownedBossSystem.downedHiveMind || npc.type == calamity.Find<ModNPC>("PerforatorHive").Type && !DownedBossSystem.downedPerforator)
-                SpawnWorldEventProjectile(ModContent.ProjectileType<GodseekerKnightProj>(), 100);
-
-            if (npc.type == NPCID.SkeletronHead && !NPC.downedBoss3)
-                Projectile.NewProjectile(Entity.GetSource_NaturalSpawn(), new Vector2(Main.dungeonX, Main.dungeonY).ToWorldCoordinates(), Vector2.Zero, ModContent.ProjectileType<LunarBishopProj>(), 0, 0, -1, 1);
 
             if (npc.type == calamity.Find<ModNPC>("SlimeGodCore").Type)
                 QuestSystem.IncrementQuestProgress(QuestSystem.QuestLog.FindIndex(quest => quest.Name == "SlimeGodHunt"), 0);
@@ -40,15 +29,6 @@ namespace Windfall.Content.NPCs.GlobalNPCs
 
             if (npc.type == calamity.Find<ModNPC>("AquaticScourgeHead").Type)
                 QuestSystem.IncrementQuestProgress(QuestSystem.QuestLog.FindIndex(quest => quest.Name == "ScoogHunt2"), 0);
-
-            if (npc.type == NPCID.Plantera && !NPC.downedPlantBoss)
-            {
-                Projectile.NewProjectile(Entity.GetSource_NaturalSpawn(), npc.Center, Vector2.Zero, ModContent.ProjectileType<OratorEntourageSpawner>(), 0, 0);
-                WorldSaveSystem.PlanteraJustDowned = true;
-            }
-
-            if (npc.type == calamity.Find<ModNPC>("LeviathanStart").Type)
-                DownedNPCSystem.downedSirenLure = true;
         }
         public override void OnSpawn(NPC npc, IEntitySource source)
         {
