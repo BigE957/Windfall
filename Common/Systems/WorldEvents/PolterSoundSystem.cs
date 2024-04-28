e6f5aad2d58d9f9b1ba676e88c62633d06bc2c59
Big E <evanmorrison42@gmail.com>
2024-04-28 13:59:41 -0400

    + Added Polterghast Ambiant noises to the Dungeon

---
+++ Common/Systems/WorldEvents/PolterSoundSystem.cs
@@ -0,0 +1,62 @@
+﻿using CalamityMod.Cooldowns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windfall.Common.Systems.WorldEvents
+{
+    public class PolterSoundSystem : ModSystem
+    {
+        public enum SystemState
+        {
+            CheckReqs,
+            CheckChance,
+            Play,
+        }
+        public SystemState State = SystemState.CheckReqs;
+
+        SoundStyle PolterAmbiance = new("Windfall/Assets/Sounds/Ambiance/Polterghast/PolterAmbiance_", 3);
+        private bool OnCooldown = false;
+        private int Counter = 30 * 60;
+        public override void PreUpdateWorld()
+        {
+            Player mainPlayer = Main.player[0];
+            if(Counter != 0)
+                DisplayLocalizedText($"{State}, {Counter}");
+            switch (State)
+            {
+                case SystemState.CheckReqs:
+                    if (mainPlayer.ZoneDungeon && !DownedBossSystem.downedPolterghast && !AnyBossNPCS() && !OnCooldown && Counter == 0)
+                        State = SystemState.CheckChance;
+                    else
+                        if (!mainPlayer.ZoneDungeon)
+                            OnCooldown = false;
+                    break;
+                case SystemState.CheckChance:
+                    if (Main.rand.NextBool(1200))
+                        State = SystemState.Play;
+                    else
+                        State = SystemState.CheckReqs;
+                    break;
+                case SystemState.Play:
+                    if (NPC.downedMoonlord)
+                        SoundEngine.PlaySound(PolterAmbiance with { Volume = 1f }, mainPlayer.Center);
+                    else if (NPC.downedPlantBoss)
+                        SoundEngine.PlaySound(PolterAmbiance with { Volume = 1f }, mainPlayer.Center + new Vector2(Main.rand.NextFloat(-1f, 1f), Main.rand.NextFloat(-1f, 1f)).SafeNormalize(Vector2.UnitX) * (Main.rand.Next(800, 1000)));
+                    else if (NPC.downedBoss3)
+                        SoundEngine.PlaySound(PolterAmbiance with { Volume = 1f }, mainPlayer.Center + new Vector2(Main.rand.NextFloat(-1f, 1f), Main.rand.NextFloat(-1f, 1f)).SafeNormalize(Vector2.UnitX) * (Main.rand.Next(1200, 1500)));
+                    else
+                        SoundEngine.PlaySound(PolterAmbiance with { Volume = 0.5f }, mainPlayer.Center + new Vector2(Main.rand.NextFloat(-1f, 1f), Main.rand.NextFloat(-1f, 1f)).SafeNormalize(Vector2.UnitX) * (Main.rand.Next(1200, 1500)));
+
+                    State = SystemState.CheckReqs;
+                    Counter = 30 * 60;
+                    break;
+            }
+
+            if(Counter > 0)
+                Counter--;
+        }
+    }
+}
