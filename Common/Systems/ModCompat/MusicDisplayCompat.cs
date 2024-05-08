69c597bae564973f18e50d25c8dec353b0958240
BigE <evanmorrison42@gmail.com>
2024-05-07 23:57:27 -0400

    + Added Music Display compatibility

---
+++ Common/Systems/ModCompat/MusicDisplayCompat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windfall.Common.Systems.ModCompat
+{
+    public class MusicDisplayCompat : ModSystem
+    {
+        public override void PostAddRecipes()
+        {
+            if (!ModLoader.TryGetMod("MusicDisplay", out Mod display))
+                return;
+
+            LocalizedText modName = Language.GetText("Mods.Windfall.MusicDisplay.ModName");
+
+            void AddMusic(string path, string name)
+            {
+                LocalizedText author = Language.GetText("Mods.Windfall.MusicDisplay." + name + ".Author");
+                LocalizedText displayName = Language.GetText("Mods.Windfall.MusicDisplay." + name + ".DisplayName");
+                display.Call("AddMusic", (short)MusicLoader.GetMusicSlot(Windfall.Instance, path), displayName, author, modName);
+            }
+
+            AddMusic("Assets/Music/Orator", "Orator");
+        }
+    }
+}
