b4912ec17fe60d8dbc312429fee72bdd2df81965
BigE <evanmorrison42@gmail.com>
2024-08-28 18:22:00 -0400

    + Orator's Boss Bar now actually uses its head texture

--- Content/UI/BossBars/OratorBossBar.cs
+++ Content/UI/BossBars/OratorBossBar.cs
@@ -5,6 +5,7 @@ namespace Windfall.Content.UI.BossBars
 {
     public class OratorBossBar : ModBossBar
     {
+        public override Asset<Texture2D> GetIconTexture(ref Rectangle? iconFrame) => ModContent.Request<Texture2D>("Windfall/Assets/NPCs/Bosses/TheOrator_Boss_Head");
         public override bool? ModifyInfo(ref BigProgressBarInfo info, ref float life, ref float lifeMax, ref float shield, ref float shieldMax)
         {
             NPC target = Main.npc[info.npcIndexToAimAt];
