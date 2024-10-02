15bad3e63089a15f13bda62ccf514c72b96d5658
Big E <evanmorrison42@gmail.com>
2024-10-02 11:00:11 -0400

    + Data Driven Dialogue is REAL

--- Common/Systems/Loader.cs
+++ Common/Systems/Loader.cs
@@ -37,58 +37,10 @@ namespace Windfall.Common.Systems
 
             #region Dialogue System
             /*
-            DialogueHolder.CharacterAssetPathes.Add($"{nameof(Windfall)}", $"{nameof(Windfall)}/Content/UI/Dialogue/CharacterAssets");
-
-            Dictionary<string, Character> Characters = new()
-            {
-                { "Windfall/TheCalamity", new Character("[c/FF0000:The Calamity]", [new Expression("Normal", 1, 0), new Expression("Finality", 1, 0)], textDelay: 3, primaryColor: Color.Black, secondaryColor: Color.Red)},
-                { "Windfall/TheOrator", new Character("[c/32CD32:The Orator]", [new Expression("Default", 1, 0)], textDelay: 3, primaryColor: Color.Black, secondaryColor: Color.LimeGreen)},
-            };
-
-            DialogueHolder.Characters.AddRange(Characters);
 
             Mod calamityMusic = ModLoader.GetMod("CalamityModMusic");
             Dictionary<string, DialogueTree> dialogueTrees = new()
             {
-                #region The Calamity Trees
-                {
-                    "Windfall/Calamitous",
-                    new DialogueTree(
-                    [
-                        new Dialogue
-                        (
-                            [
-                                new Response("What", 1, Main.LocalPlayer.direction == 1),
-                                new Response("Huh", 1, Main.LocalPlayer.direction == -1),
-                            ],
-                            expressionIndex: 0,
-                            musicID: MusicLoader.GetMusicSlot(calamityMusic, "Sounds/Music/CalamitasClone")
-                        ),
-                        new Dialogue
-                        (
-                            [
-                                new Response("Okay", -2)
-                            ],
-                            expressionIndex: 1,
-                            musicID: MusicLoader.GetMusicSlot(calamityMusic, "Sounds/Music/CalamitasClone")
-                        ),
-                        new Dialogue
-                        (
-                            expressionIndex: 1,
-                            musicID: MusicLoader.GetMusicSlot(calamityMusic, "Sounds/Music/CalamitasClone")
-                        ),
-                        new Dialogue
-                        (
-                            expressionIndex: 1,
-                            musicID: MusicLoader.GetMusicSlot(calamityMusic, "Sounds/Music/CalamitasClone")
-                        ),
-                    ],
-                    [
-                        "Windfall/TheCalamity"
-                    ]
-                    )
-                },
-                #endregion
                 #region The Chef Trees
                 {
                     "Windfall/CafeteriaActivityStart",
