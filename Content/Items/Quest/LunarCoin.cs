cd8639b186997d5a24d76159ee59be26e37d74aa
Big E <evanmorrison42@gmail.com>
2024-10-21 21:37:36 -0400

    + Added Lunar Coins

---
+++ Content/Items/Quest/LunarCoin.cs
@@ -0,0 +1,14 @@
+﻿namespace Windfall.Content.Items.Quest
+{
+    public class DeificInsignia : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Quest";
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 28;
+            Item.rare = ItemRarityID.Quest;
+            Item.maxStack = 99;
+        }
+    }
+}
