04f400c69a6c3d5b28a3f309e1d02887bfae9378
Big E <evanmorrison42@gmail.com>
2024-11-17 22:34:42 -0500

    + Fixed Selenic Order Base music themes being flipped

--- Content/Scenes/MusicScenes/EmptyBaseMusicScene.cs
+++ Content/Scenes/MusicScenes/EmptyBaseMusicScene.cs
@@ -4,7 +4,7 @@ namespace Windfall.Content.Scenes.MusicScenes;
 
 public class EmptyBaseMusicScene : ModSceneEffect
 {
-    public override int Music => MusicLoader.GetMusicSlot(Windfall.Instance, "Assets/Music/ItsRainingSomewhereElse");
-    public override bool IsSceneEffectActive(Player player) => LunarCultBaseSystem.CultBaseWorldArea.Contains(player.Hitbox) && SealingRitualSystem.RitualSequenceSeen;
+    public override int Music => MusicLoader.GetMusicSlot(Windfall.Instance, "Assets/Music/Premonition");
+    public override bool IsSceneEffectActive(Player player) => (LunarCultBaseSystem.CultBaseWorldArea.Contains(player.Hitbox) || LunarCultBaseSystem.CultBaseBridgeArea.Contains(player.Center.ToTileCoordinates())) && SealingRitualSystem.RitualSequenceSeen;
     public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
 }
--- Content/Scenes/MusicScenes/LunarCultBaseMusicScene.cs
+++ Content/Scenes/MusicScenes/LunarCultBaseMusicScene.cs
@@ -5,7 +5,7 @@ namespace Windfall.Content.Scenes.MusicScenes;
 
 public class LunarCultBaseMusicScene : ModSceneEffect
 {
-    public override int Music => MusicLoader.GetMusicSlot(Windfall.Instance, "Assets/Music/Premonition");
-    public override bool IsSceneEffectActive(Player player) => LunarCultBaseSystem.CultBaseWorldArea.Contains(player.Hitbox) && !SealingRitualSystem.RitualSequenceSeen;
+    public override int Music => MusicLoader.GetMusicSlot(Windfall.Instance, "Assets/Music/ItsRainingSomewhereElse");
+    public override bool IsSceneEffectActive(Player player) => (LunarCultBaseSystem.CultBaseWorldArea.Contains(player.Hitbox) || LunarCultBaseSystem.CultBaseBridgeArea.Contains(player.Center.ToTileCoordinates())) && !SealingRitualSystem.RitualSequenceSeen;
     public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
 }
