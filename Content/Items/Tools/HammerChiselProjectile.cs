ccb618fa00c786b91e11e8391b5b642f4d9eaa3c
Big E <evanmorrison42@gmail.com>
2024-12-23 23:44:30 -0500

    + Updated Hammer and Chisel animation

--- Content/Items/Tools/HammerChiselProjectile.cs
+++ Content/Items/Tools/HammerChiselProjectile.cs
@@ -15,7 +15,7 @@ public class HammerChiselProjectile : ModProjectile
         Projectile.height = 17;
         Projectile.penetrate = -1;
         Projectile.tileCollide = false;
-        Projectile.timeLeft = 24;
+        Projectile.timeLeft = 28;
     }
 
     private int aiCounter
@@ -32,6 +32,7 @@ public class HammerChiselProjectile : ModProjectile
     public override void OnSpawn(IEntitySource source)
     {
         spawnPos = Projectile.Center;
+        Projectile.rotation = PiOver2;
         
         if(Main.player[Projectile.owner] == Main.LocalPlayer)
         {
@@ -44,11 +45,31 @@ public class HammerChiselProjectile : ModProjectile
             }
             else
             {
-                goalPos = Main.MouseWorld.ToTileCoordinates().ToWorldCoordinates(autoAddY: 0);
+                goalPos = Main.MouseWorld.ToTileCoordinates().ToWorldCoordinates();
                 DistanceToTile = (spawnPos - goalPos).Length();
                 rotationVector = (spawnPos - goalPos).SafeNormalize(Vector2.Zero);
             }
-
+        }
+        Tile approachingTile = Main.tile[Main.MouseWorld.ToTileCoordinates()];
+        switch(approachingTile.Slope)
+        {
+            case SlopeType.SlopeDownLeft:
+                Projectile.rotation = Pi - PiOver4;
+                break;
+            case SlopeType.SlopeDownRight:
+                Projectile.rotation = PiOver4;
+                break;
+            case SlopeType.SlopeUpLeft:
+                Projectile.rotation = Pi + PiOver4;
+                break;
+            case SlopeType.SlopeUpRight:
+                Projectile.rotation = TwoPi - PiOver4;
+                break;
+            default:
+                Projectile.rotation = PiOver2;
+                if (!approachingTile.IsHalfBlock)
+                    goalPos.Y -= 8;
+                break;
         }
     }
 
@@ -56,7 +77,7 @@ public class HammerChiselProjectile : ModProjectile
     {
         if (aiCounter < 5)
             Projectile.Center = Vector2.Lerp(spawnPos, goalPos, SineOutEasing(aiCounter / 5f, 1));
-        else if (aiCounter > 21)
+        else if (aiCounter >= 24)
             Projectile.Center = Vector2.Lerp(goalPos, spawnPos, SineOutEasing((aiCounter - 20) / 4f, 1));
         else
             Projectile.Center = goalPos;
@@ -65,6 +86,39 @@ public class HammerChiselProjectile : ModProjectile
         {
             Point tileLocation = goalPos.ToTileCoordinates();
             VanillaHammerSlopingLogic(tileLocation);
+            Tile tile = Main.tile[tileLocation];
+
+            if (!tile.HasTile)
+            {
+                aiCounter++;
+                return;
+            }
+
+            Vector2 tileCenter = goalPos + Vector2.UnitY * 8f;
+            for (int i = 0; i < 10; i++) {
+                int id = i - 5;
+                switch (tile.Slope)
+                {
+                    case SlopeType.SlopeDownLeft:
+                        Dust.NewDustPerfect(tileCenter + (Vector2.One * id), DustID.GemTopaz, Vector2.One * id / 2f).noGravity = true;
+                        break;
+                    case SlopeType.SlopeUpRight:
+                        Dust.NewDustPerfect(tileCenter + (Vector2.One * id), DustID.GemTopaz, Vector2.One * id / 2f).noGravity = true;
+                        break;
+                    case SlopeType.SlopeUpLeft:
+                        Dust.NewDustPerfect(tileCenter + (new Vector2(1, -1) * id), DustID.GemTopaz, new Vector2(1, -1) * id / 2f).noGravity = true;
+                        break;
+                    case SlopeType.SlopeDownRight:
+                        Dust.NewDustPerfect(tileCenter + (new Vector2(1, -1) * id), DustID.GemTopaz, new Vector2(1, -1) * id / 2f).noGravity = true;
+                        break;
+                    default:
+                        if (tile.IsHalfBlock)
+                            Dust.NewDustPerfect(tileCenter + (Vector2.UnitX * id), DustID.GemTopaz, Vector2.UnitX * id / 2f).noGravity = true;
+                        else
+                            Dust.NewDustPerfect(goalPos + (Vector2.UnitX * id), DustID.GemTopaz, Vector2.UnitX * id / 2f).noGravity = true;
+                        break;
+                }
+            }
         }
 
         aiCounter++;
@@ -75,34 +129,40 @@ public class HammerChiselProjectile : ModProjectile
         Texture2D tex = ModContent.Request<Texture2D>(Texture).Value;
         Rectangle chiselFrame = tex.Frame(2, 1, 0, 0);
         float rotationOffset = Clamp(1 - aiCounter / 5f, 0, 1) * TwoPi;
-        if (aiCounter > 20)
-            rotationOffset = Clamp(1 - (aiCounter - 20) / 5f, 0, 1) * -TwoPi;
+        if (aiCounter >= 24)
+            rotationOffset = Clamp(1 - (aiCounter - 24) / 5f, 0, 1) * -TwoPi;
         rotationOffset *= rotationVector.X > 0 ? 1 : -1;
-        Vector2 chiselPosition = Projectile.Center - Main.screenPosition + (aiCounter > 17 ? Vector2.Zero : Vector2.UnitY * -Projectile.height);
+        Vector2 chiselPosition = Projectile.Center - Main.screenPosition + (aiCounter > 17 ? Vector2.UnitY * -Projectile.height / 3f : Vector2.UnitY * -Projectile.height).RotatedBy(Projectile.rotation - PiOver2);
 
-        Main.EntitySpriteDraw(tex, chiselPosition, chiselFrame, Color.White * Projectile.Opacity, Projectile.rotation + Pi + rotationOffset, chiselFrame.Size() * 0.5f, Projectile.scale, SpriteEffects.None);
+        Main.EntitySpriteDraw(tex, chiselPosition, chiselFrame, Color.White * Projectile.Opacity, Projectile.rotation + PiOver2 + rotationOffset, chiselFrame.Size() * 0.5f, Projectile.scale, SpriteEffects.None);
+
+        int angleNegative = rotationVector.X > 0 ? -1 : 1;
 
         Rectangle hammerFrame = tex.Frame(2, 1, 1, 0);
         Vector2 offset = new(24 * (rotationVector.X > 0 ? 1 : -1), Projectile.height * -2);
+
+
         if (aiCounter > 5)
         {
-            int angleNegative = rotationVector.X > 0 ? -1 : 1;
+            
             if (aiCounter <= 12)
             {
                 float lerp = Clamp((aiCounter - 5) / 7f, 0, 1);
                 offset += Vector2.Lerp(Vector2.Zero, new Vector2(rotationVector.X > 0 ? 24 : -24, -16), lerp);
-                rotationOffset += Lerp(0f, Pi, lerp) * angleNegative;
+                rotationOffset += Lerp(0f, -Pi, lerp) * angleNegative;
             }
             else
             {
                 float lerp = Clamp((aiCounter - 12) / 3f, 0, 1);
-                offset += Vector2.Lerp(new Vector2(rotationVector.X > 0 ? 24 : -24, -16), Vector2.UnitX * (rotationVector.X > 0 ? -16 : 16), lerp);
-                rotationOffset += Lerp(Pi, -PiOver4, lerp) * angleNegative;
+                offset += Vector2.Lerp(new Vector2(rotationVector.X > 0 ? 24 : -24, -8), Vector2.UnitX * (rotationVector.X > 0 ? -16 : 16), lerp);
+                rotationOffset += Lerp(-Pi, PiOver2, lerp) * angleNegative;
+                
             }
         }
-        Vector2 hammerPosition = (Projectile.Center - Main.screenPosition + (offset * Clamp(aiCounter / 5f, 0, 1))) + rotationVector.RotatedBy(PiOver2 * (rotationVector.X > 0 ? -1 : 1)) * (aiCounter <= 5 ? (1f - ((float)Math.Abs(aiCounter / 5f - 0.5f) * 2)) * 72 : 0);
+        
+        Vector2 hammerPosition = Projectile.Center - Main.screenPosition + (offset * Clamp(aiCounter / 5f, 0, 1)).RotatedBy(Projectile.rotation - PiOver2) + rotationVector.RotatedBy(Projectile.rotation + PiOver2 * (rotationVector.X > 0 ? -1 : 1)) * (aiCounter <= 5 ? (1f - ((float)Math.Abs(aiCounter / 5f - 0.5f) * 2)) * 72 : 0);
 
-        Main.EntitySpriteDraw(tex, hammerPosition, hammerFrame, Color.White * Projectile.Opacity, Projectile.rotation - rotationOffset + (PiOver4 / 2f * rotationVector.X > 0 ? -1 : 1), chiselFrame.Size() * 0.5f, Projectile.scale, rotationVector.X > 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+        Main.EntitySpriteDraw(tex, hammerPosition, hammerFrame, Color.White * Projectile.Opacity, Projectile.rotation + rotationOffset + (-PiOver2 * angleNegative), chiselFrame.Size() * 0.5f, Projectile.scale, rotationVector.X > 0 ? SpriteEffects.FlipVertically : SpriteEffects.None);
 
         return false;
     }
