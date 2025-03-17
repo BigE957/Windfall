8ffa8f5fc7810f7d174f30780c064ec8c2a6596a
Big E <evanmorrison42@gmail.com>
2025-03-17 11:43:11 -0400

    + Added Wayfinder Functionality and updated Quest Dialogue

--- Content/Items/Quests/SealingRitual/UnchargedWayfinder.cs
+++ Content/Items/Quests/SealingRitual/UnchargedWayfinder.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using CalamityMod.Tiles.Ores;
+
 namespace Windfall.Content.Items.Quests.SealingRitual;
 
 public class UnchargedWayfinder : ModItem, ILocalizedModType
@@ -9,13 +10,83 @@ public class UnchargedWayfinder : ModItem, ILocalizedModType
     {
         Item.width = 22;
         Item.height = 24;
+        Item.useAnimation = 2;
+        Item.useTime = 2;
+        Item.autoReuse = true;
+        Item.useStyle = ItemUseStyleID.HoldUp;
+        Item.channel = true;
         Item.rare = ItemRarityID.Quest;
-        Item.maxStack = 99;
+        Item.maxStack = 1;
     }
 
-    public override bool CanRightClick() => true;
-    public override void RightClick(Player player)
+    private List<int> DustIndexes = [];
+    private Point currentPoint = new(-1, -1);
+    private int holdTime = 0;
+    private int charge = 0;
+
+    public override bool? UseItem(Player player)
     {
-        Item.type = ModContent.ItemType<Wayfinder>(); 
+        Vector2 toMouse = (player.Calamity().mouseWorld - player.Center);
+        float dist = toMouse.Length();
+        toMouse = toMouse.SafeNormalize(Vector2.Zero);
+
+        player.ChangeDir(Math.Sign(toMouse.X));
+
+        if (player.Calamity().mouseWorld.ToTileCoordinates() != currentPoint)
+        {
+            currentPoint = player.Calamity().mouseWorld.ToTileCoordinates();
+            holdTime = 0;
+        }
+
+        Tile hoverTile = Main.tile[currentPoint];
+        if(hoverTile != null && hoverTile.TileType == ModContent.TileType<AerialiteOre>())
+        {
+            if(holdTime >= 120)
+            {
+                Main.tile[currentPoint].ResetToType((ushort)ModContent.TileType<AerialiteOreDisenchanted>());
+                WorldGen.KillTile(currentPoint.X, currentPoint.Y, fail: true);
+                charge++;
+                holdTime = 0;
+                for (int i = 0; i < 12; i++)
+                    Dust.NewDustPerfect(player.Calamity().mouseWorld, DustID.BlueFairy);
+                if (charge >= 10)
+                    Item.ChangeItemType(ModContent.ItemType<Wayfinder>());
+                return true;
+            }
+            if ((int)(Main.GlobalTimeWrappedHourly * 60) % 2 == 0)
+            {
+                Dust d = Dust.NewDustPerfect(player.Calamity().mouseWorld, DustID.BlueFairy, toMouse * -4);
+                d.scale = Clamp(dist / 150f, 0.5f, 2f);
+                DustIndexes.Add(d.dustIndex);
+            }
+            if (Main.rand.NextBool(4))
+            {
+                Dust d = Dust.NewDustPerfect(player.Calamity().mouseWorld, DustID.BlueFairy);
+                d.velocity /= 2f;
+            }
+            holdTime++;
+        }
+        return base.UseItem(player);
+    }
+
+    public override void HoldItem(Player player)
+    {
+        if(DustIndexes == null)
+            DustIndexes = [];
+        foreach (int i in DustIndexes)
+        {
+            Vector2 toPlayer = (player.Center - Main.dust[i].position);
+            float dist = Vector2.Distance(Main.dust[i].position, player.Center);
+            if (dist < 16)
+            {
+                Main.dust[i].active = false;
+                continue;
+            }
+            toPlayer = toPlayer.SafeNormalize(Vector2.Zero);
+            Main.dust[i].velocity = toPlayer * 4;
+            Main.dust[i].scale = Clamp(dist / 150f, 0.5f, 2f);
+        }
+
+        DustIndexes.RemoveAll(i => !Main.dust[i].active);
     }
 }
--- Content/Items/Quests/SealingRitual/Wayfinder.cs
+++ Content/Items/Quests/SealingRitual/Wayfinder.cs
@@ -1,4 +1,8 @@
-﻿namespace Windfall.Content.Items.Quests.SealingRitual;
+﻿
+using Windfall.Common.Systems.WorldEvents;
+using Terraria.ID;
+
+namespace Windfall.Content.Items.Quests.SealingRitual;
 
 public class Wayfinder : ModItem, ILocalizedModType
 {
@@ -8,7 +12,21 @@ public class Wayfinder : ModItem, ILocalizedModType
     {
         Item.width = 22;
         Item.height = 24;
+        Item.useAnimation = 12;
+        Item.useTime = 12;
+        Item.autoReuse = false;
+        Item.useStyle = ItemUseStyleID.HoldUp;
+        Item.channel = false;
         Item.rare = ItemRarityID.Quest;
-        Item.maxStack = 99;
+        Item.maxStack = 1;
+        Item.shoot = ProjectileID.WoodenArrowFriendly; //Does not matter, only here so that Shoot gets called
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        Vector2 toRuins = (DraconicRuinsSystem.RuinsEntrance.ToWorldCoordinates() - player.Center).SafeNormalize(Vector2.Zero);
+        Particle particle = new DirectionalPulseRing(player.Center, toRuins * 7f, Color.Cyan, new(1, 0.66f), toRuins.ToRotation(), 0f, 1.5f, 24);
+        GeneralParticleHandler.SpawnParticle(particle);
+        return false;
     }
 }
