87507464654f9fcac055cdaa73e9132fc2eb664c
Big E <evanmorrison42@gmail.com>
2025-04-29 20:09:29 -0400

    + Using clean up

--- Content/Tiles/Blocks/WornSlabTile.cs
+++ Content/Tiles/Blocks/WornSlabTile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Windfall.Content.Tiles.Blocks;
+﻿namespace Windfall.Content.Tiles.Blocks;
 public class WornSlabTile : ModTile
 {
     public override void SetStaticDefaults()
