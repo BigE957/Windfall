21abc04fe5d88eab005ae7c5cfc0556f68d3df24
Big E <evanmorrison42@gmail.com>
2025-04-29 20:08:36 -0400

    + Added Worn Slabs

---
+++ Content/Items/Placeables/Tiles/WornSlab.cs
@@ -0,0 +1,34 @@
+﻿using Windfall.Content.Tiles.Blocks;
+
+namespace Windfall.Content.Items.Placeables.Tiles;
+public class WornSlab : ModItem, ILocalizedModType
+{
+    public new string LocalizationCategory => "Items.Placeables";
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 100;
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 12;
+        Item.height = 12;
+        Item.maxStack = 9999;
+        Item.useTurn = true;
+        Item.autoReuse = true;
+        Item.useAnimation = 15;
+        Item.useTime = 10;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.consumable = true;
+        Item.createTile = ModContent.TileType<WornSlabTile>();
+    }
+
+    public override void AddRecipes()
+    {
+        CreateRecipe(100).
+            AddIngredient(ItemID.StoneSlab, 100).
+            AddIngredient(ItemID.BrownDye).
+            AddTile(TileID.DyeVat).
+            Register();
+    }
+}
---
+++ Content/Tiles/Blocks/WornSlabTile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windfall.Content.Tiles.Blocks;
+public class WornSlabTile : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = true;
+        Main.tileLargeFrames[Type] = 1;
+        Main.tileMergeDirt[Type] = false;
+        Main.tileBlockLight[Type] = true;
+        HitSound = SoundID.Tink;
+        AddMapEntry(Color.Brown);
+    }
+
+    public override bool CreateDust(int i, int j, ref int type)
+    {
+        Dust.NewDust(new Vector2(i, j) * 16f, 16, 16, DustID.UnusedBrown);
+        return false;
+    }
+}
