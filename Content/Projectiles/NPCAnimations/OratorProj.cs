abff7d3a7061ed0229dc177bf4fd04c6af9d60f3
Big E <evanmorrison42@gmail.com>
2025-05-03 10:51:45 -0400

    + Moved Post-Plantera combat text dialogue to the Dialogue System

--- Content/Projectiles/NPCAnimations/OratorProj.cs
+++ Content/Projectiles/NPCAnimations/OratorProj.cs
@@ -1,30 +1,16 @@
 ﻿using CalamityMod.Dusts;
+using DialogueHelper.UI.Dialogue;
+using Windfall.Content.Items.Lore;
 using Windfall.Content.Items.Quests;
 using Windfall.Content.NPCs.WorldEvents.LunarCult;
 
 namespace Windfall.Content.Projectiles.NPCAnimations;
 
-public class OratorProj : ProjectileNPC, ILocalizedModType
+public class OratorProj : ModProjectile, ILocalizedModType
 {
     public override string Texture => "Windfall/Assets/NPCs/WorldEvents/TheOrator_NPC";
-    internal override string key => "LunarCult.TheOrator.WorldText.Post-Plant";
-    internal override List<float> Delays =>
-    [
-        1,
-        3,
-        3,
-        3,
-        4,
-        3,
-        2,
-        3,
-        3,
-        3,
-    ];
-    internal override SoundStyle SpawnSound => new("CalamityMod/Sounds/Custom/SCalSounds/BrimstoneHellblastSound");
-    internal override int NPCType => -1;
-    internal override Color TextColor => Color.LightGreen;
-    internal override void DoOnSpawn()
+    internal SoundStyle SpawnSound => new("CalamityMod/Sounds/Custom/SCalSounds/BrimstoneHellblastSound");
+    public override void OnSpawn(IEntitySource source)
     {
         for (int i = 0; i <= 50; i++)
         {
@@ -36,34 +22,6 @@ public class OratorProj : ProjectileNPC, ILocalizedModType
         }
         SoundEngine.PlaySound(SpawnSound, Projectile.Center);
     }
-    internal override void DoBeforeDespawn()
-    {
-        Item i = Main.item[Item.NewItem(Entity.GetSource_Loot(), Projectile.Center, new Vector2(8, 4), ModContent.ItemType<SelenicTablet>())];
-        i.velocity = new Vector2(Projectile.direction, 0) * -4;
-    }
-    internal override void DoOnDespawn()
-    {
-        foreach (NPC npc in Main.npc.Where(n => (n.type == ModContent.NPCType<LunarCultistArcher>() || n.type == ModContent.NPCType<LunarCultistDevotee>()) && n.active))
-        {
-            for (int i = 0; i < 50; i++)
-            {
-                Vector2 speed = Main.rand.NextVector2Circular(1.5f, 2f);
-                Dust d = Dust.NewDustPerfect(npc.Center, DustID.GoldFlame, speed * 3, Scale: 1.5f);
-                d.noGravity = true;
-            }
-            SoundEngine.PlaySound(SpawnSound, npc.Center);
-            npc.active = false;
-        }
-        SoundEngine.PlaySound(SpawnSound, Projectile.Center);
-        for (int i = 0; i < 75; i++)
-        {
-            Vector2 speed = Main.rand.NextVector2Circular(1.5f, 2f);
-            Dust d = Dust.NewDustPerfect(Projectile.Center, (int)CalamityDusts.Ectoplasm, speed * 2, Scale: 1.5f);
-            d.noGravity = true;
-        }
-        Main.npc[NPC.FindFirstNPC(ModContent.NPCType<LunarBishop>())].ai[2] = 1;
-    }
-    internal override int DespawnDelay => 60 * 3;
     public override void SetStaticDefaults()
     {
         Main.projFrames[Type] = 1;
@@ -79,5 +37,47 @@ public class OratorProj : ProjectileNPC, ILocalizedModType
         Projectile.timeLeft = int.MaxValue;
         Projectile.alpha = 255;
         Projectile.ai[0] = 0;
+    }
+
+    private int Time = 0;
+
+    public override void AI()
+    {
+        DialogueUISystem uiSystem = ModContent.GetInstance<DialogueUISystem>();
+        if (Time == 60)
+            uiSystem.DisplayDialogueTree(Windfall.Instance, "Cutscenes/SelenicEncounter", new(Name, [Projectile.whoAmI]), 5);
+        else if (!uiSystem.isDialogueOpen)
+        {
+            if (Time == 120)
+            {
+                Item i = Main.item[Item.NewItem(Entity.GetSource_Loot(), Projectile.Center, new Vector2(8, 4), ModContent.ItemType<SelenicTablet>())];
+                i.velocity = new Vector2(Projectile.direction, 0) * -4;
+            }
+            if (Time > 240)
+            {
+                foreach (NPC npc in Main.npc.Where(n => (n.type == ModContent.NPCType<LunarCultistArcher>() || n.type == ModContent.NPCType<LunarCultistDevotee>()) && n.active))
+                {
+                    for (int i = 0; i < 50; i++)
+                    {
+                        Vector2 speed = Main.rand.NextVector2Circular(1.5f, 2f);
+                        Dust d = Dust.NewDustPerfect(npc.Center, DustID.GoldFlame, speed * 3, Scale: 1.5f);
+                        d.noGravity = true;
+                    }
+                    SoundEngine.PlaySound(SpawnSound, npc.Center);
+                    npc.active = false;
+                }
+                SoundEngine.PlaySound(SpawnSound, Projectile.Center);
+                for (int i = 0; i < 75; i++)
+                {
+                    Vector2 speed = Main.rand.NextVector2Circular(1.5f, 2f);
+                    Dust d = Dust.NewDustPerfect(Projectile.Center, (int)CalamityDusts.Ectoplasm, speed * 2, Scale: 1.5f);
+                    d.noGravity = true;
+                }
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<LunarBishop>())].ai[2] = 1;
+                Projectile.active = false;
+            }
+        }
+        if(Time < 60 || !uiSystem.isDialogueOpen)
+            Time++;
     }
 }
