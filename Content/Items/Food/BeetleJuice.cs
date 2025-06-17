8249a931ff9b68af4712cb0f9c6509ebc362c3a8
Big E <evanmorrison42@gmail.com>
2025-06-17 18:36:17 -0400

    + Re-added Beetle Juice

---
+++ Content/Items/Food/BeetleJuice.cs
@@ -0,0 +1,34 @@
+﻿using CalamityMod.Buffs.StatBuffs;
+
+namespace Windfall.Content.Items.Food;
+public class BeetleJuice : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 5;
+
+        // Dust that will appear in these colors when the item with ItemUseStyleID.DrinkLiquid is used
+        ItemID.Sets.DrinkParticleColors[Type] = [
+            new Color(240, 240, 240),
+            new Color(200, 200, 200),
+            new Color(140, 140, 140)
+        ];
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 20;
+        Item.height = 26;
+        Item.useStyle = ItemUseStyleID.DrinkLiquid;
+        Item.useAnimation = 15;
+        Item.useTime = 15;
+        Item.useTurn = true;
+        Item.UseSound = SoundID.Item3;
+        Item.maxStack = Item.CommonMaxStack;
+        Item.consumable = true;
+        Item.rare = ItemRarityID.Orange;
+        Item.value = Item.buyPrice(gold: 1);
+        Item.buffType = ModContent.BuffType<AbsorberRegen>();
+        Item.buffTime = 5400;
+    }
+}
